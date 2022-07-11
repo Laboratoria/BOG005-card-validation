7cefe4f7d104e5466e65964c237afbe0e34f3438
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-07-11 18:26:34 -0500

    estructura con la imagen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//ingresando variable para la imagen 
+var foto = document.getElementById("motoycarro");
+foto.src= "motoycarro".jpg; 
+
 import validator from './validator.js';
 
 console.log(validator);
