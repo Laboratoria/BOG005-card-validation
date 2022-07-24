d450230cf3ea76c13abb38650459310debb53c49
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-07-23 23:45:19 -0500

    validator, css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-//ingresando variable para la imagen 
-var foto = document.getElementById("motoycarro");
-foto.src= "motoycarro".jpg; 
-
 import validator from './validator.js';
 
-console.log(validator);
+
+//Guardar el numero de la tarjeta
+document.getElementById("enviar").addEventListener("click", mostrarSeleccionado)
+function mostrarSeleccionado() {
+let creditCard1= document.getElementById("creditCard1").value;
+console.log(creditCard1)
+validator.isValid(creditCard1)
+if (creditCard1 === true)
+document.write( "Tarjeta Valida");
+else{
+    document.write("Su tarjeta no es Valida")
+}
+//Boton del seleccionador de metodos de pago:
+document.getElementById("botonSeleccion").onclick = function(){
+    alert("Por favor llena el formulario");
+}
+
+//Funcion de replace numbers
+let maskifyNumbers= validator.maskify(creditCard1);
+result.innerText= "Su tarjeta número"+ maskifyNumbers + " "+ textR;
+
+}
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
-const validator = {
-  // ...
-};
 
+let validator = {
+  isValid: (creditCardNumber) => {
+
+    let reverseTarjeta = creditCardNumber.toString().split("").reverse();
+    console.log(reverseTarjeta)
+    let valor = 0;
+    reverseTarjeta.forEach((item, index) => {
+      console.log("ini", item)
+      //donde index toma las posiciones pares 
+      if (index % 2 === 0) {
+        let pairNumber = item * 2
+        //donde item multiplica por dos las posiciones par
+        if (pairNumber > 9) {
+
+          let digits = pairNumber.toString().split("")
+          let suma = parseInt(digits[0]) + parseInt(digits[1])
+          item = suma
+          console.log("resultadosuma", item)
+          valor = valor + item;
+
+          console.log("despues", valor)
+        } else {
+          let numberNuevo = item * 2
+          //numberNuevo son aquellos numeros en la posicion par que no eran mayores a 10. 
+          valor = valor + numberNuevo;
+        }
+
+      } else {
+        valor = valor + parseInt(item);
+      } 
+          
+    }) 
+    if( valor % 10 === 0){
+      return true;
+    } else {
+      return false;
+    }
+    
+  },
+  maskify:(creditCardNumber) => {
+  let maskify= " ";
+  for (let i = 0; i <= creditCardNumber.length; i++) {
+    if (i <= creditCardNumber.length - 4) {
+      maskify = maskify + "#"
+    } else {
+      maskify = maskify + creditCardNumber[i];
+    }
+  } console.log(maskify)
+  return maskify;
+} 
+}
+
+// ...  
 export default validator;
